e324850be53942c795e9db840ebc5960c03d85aa
baitsalem <baitsalem@gmail.com>
2019-05-29 10:49:10 +0200

    add cmonwirelessdevice

--- src/vwifi-inet-monitor.cc
+++ src/vwifi-inet-monitor.cc
@@ -1,16 +1,26 @@
 #include "cwirelessdevice.h"
 
+#include <stdio.h>
 #include <iostream>
 
 int main (int agc, char ** argv){
 
 
 	struct ether_addr mac { 0xff, 0x00, 0x00,0x00, 0xfe, 0x00 };
-	
+	struct ether_addr mac3 { 0xaa, 0xbb, 0x00,0x00, 0xfe, 0x00 };
+
 	WirelessDevice wdevice("wlan0",4,5,mac);
 	std::cout << wdevice << std::endl ;
 
- 	return 0 ;
+	WirelessDevice wdevice2 ;
+	wdevice2 = wdevice;
+	
+	wdevice.setMacaddr(mac3);
+	
+	std::cout << wdevice2 << std::endl ;
+	std::cout << wdevice << std::endl ;
+
+	return 0 ;
 
 
 
