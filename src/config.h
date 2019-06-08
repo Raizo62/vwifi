2b3188ac57e5440e0c6c8064500345d56a910055
raizo62 <david.ansart@cfautec.fr>
2019-06-08 19:44:38 +0200

    config.h : use the types Number and TPort, instead of int

--- src/config.h
+++ src/config.h
@@ -1,10 +1,13 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-const int MAX_CLIENT_WIFI = 30;
+#include "csocket.h" // TPort
+#include "csocketserver.h" // Number
 
-const int WIFI_PORT = 8211;
-const int CTRL_PORT = 8888;
+const Number MAX_CLIENT_WIFI = 30;
+
+const TPort WIFI_PORT = 8211;
+const TPort CTRL_PORT = 8888;
 
 //#define ADDRESS_IP "127.0.0.1"
 #define ADDRESS_IP "192.168.56.101"
