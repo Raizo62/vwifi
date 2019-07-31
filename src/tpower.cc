15390f7deb9433be4adbdcbfefc862ac9751e47e
raizo62 <david.ansart@cfautec.fr>
2019-07-31 11:28:06 +0200

    tpower : add function to calculate the Attenuation of the signal

--- src/tpower.cc
+++ src/tpower.cc
@@ -1,8 +1,16 @@
 #include <cstdio> //perror
 
 #include <iostream> // cout
+#include <math.h>   // log10
 
 #include "tpower.h"
 
 using namespace std;
+using namespace power;
+
+// distance : meter
+int power::Attenuation(float distance)
+{
+	return ConstanteC+20*log10(Frequency)+20*log10(distance/1000);
+}
 
--- src/tpower.h
+++ src/tpower.h
@@ -1,8 +1,16 @@
 #ifndef _TPOWER_H_
 #define _TPOWER_H_
-
 #include "types.h" // TPower
 
+namespace power
+{
 
+const float ConstanteC=92.45;
+const float Frequency=2.4; // GHz
+
+// distance : meter
+int Attenuation(float distance);
+
+}
 
 #endif
