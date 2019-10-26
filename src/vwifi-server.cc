29d0957259df3ae402067b10a008378d46c12e2c
Raizo62 <david.ansart@cfautec.fr>
2019-10-26 18:17:33 +0200

    vwifi-server : accept the connection of vwifi-host

--- src/vwifi-server.cc
+++ src/vwifi-server.cc
@@ -30,6 +30,16 @@ int main(int argc , char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	cout<<"HOST : ";
+	CWifiServer wifiHostServer(AF_INET);
+	wifiHostServer.SetPacketLoss(false);
+	wifiHostServer.Init(WIFI_PORT);
+	if( ! wifiHostServer.Listen(1) )
+	{
+		cerr<<"Error : wifiHostServer.Listen"<<endl;
+		exit(EXIT_FAILURE);
+	}
+
 	cout<<"CTRL : ";
 	CCTRLServer ctrlServer(&wifiGuestServer,&scheduler);
 	ctrlServer.Init(CTRL_PORT);
@@ -48,6 +58,7 @@ int main(int argc , char *argv[])
 
 	//add master socket to set
 	scheduler.AddNode(wifiGuestServer);
+	scheduler.AddNode(wifiHostServer);
 	scheduler.AddNode(ctrlServer);
 
 	while( true )
@@ -76,7 +87,42 @@ int main(int argc , char *argv[])
 				scheduler.AddNode(socket);
 
 				//inform user of socket number - used in send and receive commands
-				cout<<"New connection from : "; wifiGuestServer.ShowInfoWifi(wifiGuestServer.GetNumberClient()-1) ; cout<<endl;
+				cout<<"New connection from Guest : "; wifiGuestServer.ShowInfoWifi(wifiGuestServer.GetNumberClient()-1) ; cout<<endl;
+			}
+
+			if( scheduler.NodeHasAction(wifiHostServer) )
+			{
+				socket = wifiHostServer.Accept();
+				if ( socket == SOCKET_ERROR )
+				{
+					cerr<<"Error : wifiHostServer.Accept"<<endl;
+					exit(EXIT_FAILURE);
+				}
+
+				//add child sockets to set
+				scheduler.AddNode(socket);
+
+				//inform user of socket number - used in send and receive commands
+				cout<<"New connection from Host"<<endl;
+			}
+
+			if( scheduler.NodeHasAction(wifiHostServer) )
+			{
+				socket = wifiHostServer[0];
+
+				if( ! wifiHostServer.IsEnable(0) )
+				{
+							//Somebody disconnected , get his details and print
+							cout<<"Host disable : "<<endl;
+
+							//Close the socket
+							wifiHostServer.CloseClient(0);
+
+							//del master socket to set
+							scheduler.DelNode(socket);
+
+							continue;
+				}
 			}
 
 			if( scheduler.NodeHasAction(ctrlServer) )
@@ -92,7 +138,7 @@ int main(int argc , char *argv[])
 				if( ! wifiGuestServer.IsEnable(i) )
 				{
 							//Somebody disconnected , get his details and print
-							cout<<"Host disable : "; wifiGuestServer.ShowInfoWifi(i) ; cout<<endl;
+							cout<<"Guest disable : "; wifiGuestServer.ShowInfoWifi(i) ; cout<<endl;
 
 							//Close the socket
 							wifiGuestServer.CloseClient(i);
@@ -152,8 +198,69 @@ int main(int argc , char *argv[])
 							cout<<"Forward "<<valread<<" bytes from "; wifiGuestServer.ShowInfoWifi(i); cout<<" to "<< wifiGuestServer.GetNumberClient()-1 << " others clients" <<endl;
 #endif
 							wifiGuestServer.SendAllOtherClients(i,power,buffer,valread);
+							wifiHostServer.SendAllClientsWithoutLoss(power,buffer,valread);
 						}
 
+					}
+				}
+
+				i++;
+			}
+
+						//else its some IO operation on some other socket
+			for ( i = 0 ; i < wifiHostServer.GetNumberClient() ; )
+			{
+				socket = wifiHostServer[i];
+
+				if( ! wifiHostServer.IsEnable(i) )
+				{
+							//Somebody disconnected , get his details and print
+							cout<<"Host disable : "; wifiHostServer.ShowInfoWifi(i) ; cout<<endl;
+
+							//Close the socket
+							wifiHostServer.CloseClient(i);
+
+							//del master socket to set
+							scheduler.DelNode(socket);
+
+							continue;
+				}
+
+				if( scheduler.NodeHasAction(socket) )
+				{
+					//Check if it was for closing , and also read the
+					//incoming message
+
+					// read the power
+					valread = wifiHostServer.Read( socket , (char*)&power, sizeof(power));
+					if ( valread >= 0 )
+					{
+						if ( valread == 0 )
+						{
+							//Close the socket
+							wifiHostServer.CloseClient(i);
+
+							//del master socket to set
+							scheduler.DelNode(socket);
+
+							continue;
+						}
+					}
+
+					// read the data
+					valread = wifiHostServer.Read( socket , buffer, sizeof(buffer));
+					if ( valread >= 0 )
+					{
+						if ( valread == 0 )
+						{
+							//Close the socket
+							wifiHostServer.CloseClient(i);
+
+							//del master socket to set
+							scheduler.DelNode(socket);
+
+							continue;
+						}
 					}
 				}
 
