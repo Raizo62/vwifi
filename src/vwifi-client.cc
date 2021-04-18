2c3058aa567cfec7aec5abbf4a036d0992e311f0
Raizo62 <silicium62-github@yahoo.fr>
2021-04-18 13:23:56 +0200

    vwifi-client : add options to set the Port of vwifi-server : 	[--port PORT] 	[-p PORT]

--- src/vwifi-client.cc
+++ src/vwifi-client.cc
@@ -11,7 +11,7 @@ enum STATE {
 
 	STARTED=1,
 	STOPPED ,
-	SUSPENDED 
+	SUSPENDED
 };
 
 CKernelWifi* wifiClient;
@@ -27,7 +27,7 @@ void  signal_handler(int signal_num)
 		case SIGINT :
 		case SIGTERM :
 		case SIGQUIT :
-			
+
 			std::cout << signal_num << std::endl ;
 			wifiClient->stop() ;
 			_state = STOPPED ;
@@ -37,16 +37,21 @@ void  signal_handler(int signal_num)
 
 			std::cout << "This signal is ignored" << std::endl ;
 			break ;
-		
+
 
 		default :
 			std::cerr << "Signal not handled" << std::endl ;
-	
+
 	}
 
 	std::cout << "OUT SWITCH" << std::endl ;
 }
 
+void help()
+{
+	std::cout<<"Usage: vwifi-client [-h] [-v] [IP_ADDR] [-p PORT]"<<std::endl;
+	std::cout<<"                    [--help] [--version] [IP_ADDR] [--port PORT]"<<std::endl;
+}
 
 int main (int argc , char ** argv){
 
@@ -58,27 +63,57 @@ int main (int argc , char ** argv){
 	signal(SIGTSTP, signal_handler);
 	//signal(SIGCONT, signal_handler);
 
-	if( argc > 2 )
-	{
-		std::cerr<<"Error : too many parameters"<<std::endl;
-		return 1;
-	}
+	std::string ip_addr;
+	TPort port_number = -1;
 
-	if( argc == 2 )
+	int arg_idx = 1;
+	while (arg_idx < argc)
 	{
-		if( ! strcmp("-v", argv[1]) || ! strcmp("--version", argv[1]) )
+		if( ! strcmp("-v", argv[arg_idx]) || ! strcmp("--version", argv[arg_idx]) )
 		{
 			std::cout<<"Version : "<<VERSION<<std::endl;
 			return 0;
 		}
+		if( ! strcmp("-h", argv[arg_idx]) || ! strcmp("--help", argv[arg_idx]) )
+		{
+			help();
+			return 0;
+		}
+		if( ( ! strcmp("-p", argv[arg_idx]) || ! strcmp("--port", argv[arg_idx]) ) && (arg_idx + 1) < argc)
+		{
+			port_number = std::stoi(argv[arg_idx+1]);
+			arg_idx++;
+		}
+		else
+		{
+			if( ip_addr.empty() )
+				ip_addr = std::string(argv[arg_idx]);
+			else
+			{
+				std::cerr<<"Error : unknown parameter : "<< argv[arg_idx] <<std::endl;
+				help();
+				return 1;
+			}
+		}
 
-		wifiClient=new CWifiClient<CSocketClientINET>;
-		((CWifiClient<CSocketClientINET>*)wifiClient)->Init(argv[1], WIFI_CLIENT_PORT_INET);
+		arg_idx++;
+	}
+
+	if( ip_addr.empty() )
+	{ // IP not set -> mode VHOST
+		if( port_number  == -1 )
+			port_number = WIFI_CLIENT_PORT_VHOST;
+
+		wifiClient=new CWifiClient<CSocketClientVHOST>;
+		((CWifiClient<CSocketClientVHOST>*)wifiClient)->Init(port_number);
 	}
 	else
-	{
-		wifiClient=new CWifiClient<CSocketClientVHOST>;
-		((CWifiClient<CSocketClientVHOST>*)wifiClient)->Init(WIFI_CLIENT_PORT_VHOST);
+	{ // mode TCP
+		if( port_number == -1 )
+			port_number = WIFI_CLIENT_PORT_INET;
+
+		wifiClient=new CWifiClient<CSocketClientINET>;
+		((CWifiClient<CSocketClientINET>*)wifiClient)->Init(ip_addr.c_str(), port_number);
 	}
 
 	if(!wifiClient->start())
@@ -86,7 +121,7 @@ int main (int argc , char ** argv){
 
 
 
-	std::cout << "Good Bye (:-)" << std::endl ; 
+	std::cout << "Good Bye (:-)" << std::endl ;
 
 	_exit(EXIT_SUCCESS);
 
