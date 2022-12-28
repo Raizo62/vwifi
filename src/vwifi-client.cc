e7e3481bbc9b96f43ab7f0aa78c68f477e805b53
Raizo62 <silicium62-github@yahoo.fr>
2022-12-28 13:40:45 +0100

    vwifi-client : - add parameter "--number" ("-n") and "--mac" ("-m") : 		-- -n : choose the number of interface to add 		-- -m : set the mac prefix address to use The default values are chosen in the file "config.h" : 	-- DEFAULT_NUMBER_WLAN_INTERFACE=0; 	-- DEFAULT_MAC_PREFIX "74:F8:F6"

--- src/vwifi-client.cc
+++ src/vwifi-client.cc
@@ -13,6 +13,8 @@
 #endif
 #include "csocketclientitcp.h"
 
+#include "addinterfaces.h"
+
 enum STATE {
 
 	STARTED=1,
@@ -55,11 +57,12 @@ void  signal_handler(int signal_num)
 
 void help()
 {
-	std::cout<<"Usage: vwifi-client [-h] [-v] [-s] [IP_ADDR] [-p PORT]"<<std::endl;
-	std::cout<<"                    [--help] [--version] [--spy] [IP_ADDR] [--port PORT]"<<std::endl;
+	std::cout<<"Usage: vwifi-client [-h] [-v] [-s] [IP_ADDR] [-p PORT] [-n NUMBER] [-m MAC_PREFIX]"<<std::endl;
+	std::cout<<"                    [--help] [--version] [--spy] [IP_ADDR] [--port PORT] [--number NUMBER] [--mac MAC_PREFIX]"<<std::endl;
 	std::cout<<"         By default : client mode : TCP : IP_ADDR="<<DEFAULT_ADDRESS_IP <<" PORT="<< DEFAULT_WIFI_CLIENT_PORT_INET << std::endl;
 	std::cout<<"                      client mode : VHOST : PORT="<< DEFAULT_WIFI_CLIENT_PORT_VHOST << std::endl;
 	std::cout<<"                      spy mode (--spy) : IP_ADDR="<< DEFAULT_ADDRESS_IP <<" PORT="<< DEFAULT_WIFI_SPY_PORT << std::endl;
+	std::cout<<"                      NUMBER="<< DEFAULT_NUMBER_WLAN_INTERFACE <<" MAC_PREFIX="<< DEFAULT_MAC_PREFIX << std::endl;
 }
 
 int main (int argc , char ** argv){
@@ -68,6 +71,15 @@ int main (int argc , char ** argv){
 
 	std::string ip_addr;
 	TPort port_number = 0;
+
+	int number_interface=DEFAULT_NUMBER_WLAN_INTERFACE;
+	TByte mac_prefix[ETH_ALEN]={};
+	ParseAddress(DEFAULT_MAC_PREFIX, mac_prefix);
+	if( strlen(DEFAULT_MAC_PREFIX) <= 8 )
+	{ // if possible, randomize the 4th byte
+		srand(time(NULL));
+		mac_prefix[3]=rand()%100;
+	}
 
 	int arg_idx = 1;
 	while (arg_idx < argc)
@@ -91,6 +103,33 @@ int main (int argc , char ** argv){
 		{
 			spy=true;
 		}
+		else if( ( ! strcmp("-n", argv[arg_idx]) || ! strcmp("--number", argv[arg_idx]) ) && (arg_idx + 1) < argc)
+		{
+			number_interface = std::stoi(argv[arg_idx+1]);
+			if (number_interface < 0)
+			{
+				std::cerr<<"Error : NUMBER < 0"<<std::endl;
+				return 3;
+			}
+			if (number_interface > 100)
+			{
+				std::cerr<<"Error : NUMBER > 100"<<std::endl;
+				return 4;
+			}
+			arg_idx++;
+		}
+		else if( ( ! strcmp("-m", argv[arg_idx]) || ! strcmp("--mac", argv[arg_idx]) ) && (arg_idx + 1) < argc)
+		{
+			std::string string = std::string(argv[arg_idx+1]);
+			if( string.size() > 17 )
+			{
+				std::cerr<<"Error : the MAC_PREFIX is too long"<<std::endl;
+				return 5;
+			}
+			if( ! ParseAddress(string.c_str(), mac_prefix) )
+				return 6;
+			arg_idx++;
+		}
 		else
 		{
 			if( ip_addr.empty() )
@@ -111,6 +150,10 @@ int main (int argc , char ** argv){
 		std::cerr<<"Error : This program must be run as root!!"<<std::endl;
 		return 2;
 	}
+
+	if( number_interface )
+		if( ManageRadios(number_interface,mac_prefix) )
+			return 7;
 
 	/* Handle signals */
 	signal(SIGINT, signal_handler);
@@ -146,7 +189,7 @@ int main (int argc , char ** argv){
 			static_cast<CWifiClient<CSocketClientVTCP>*>(wifiClient)->Init(port_number);
 #else
 			std::cerr<<"Error : This program is not build with VHOST!!"<<std::endl;
-			return 3;
+			return 8;
 #endif
 		}
 		else
